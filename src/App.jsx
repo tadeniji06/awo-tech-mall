10aae6e3fe28e1508cf207b44b91bdd36d915c8e
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-07-11 13:40:06 +0100

    Add Microsoft Clarity for enhanced analytics and tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import AppRoutes from "./routes/routes";
-
+import AppRoutes from './routes/routes';
+import { useEffect } from 'react';
+import Clarity from '@microsoft/clarity';
 const App = () => {
+	const projectId = 'sd5tdu9giq';
+	useEffect(() => {
+		Clarity.init(projectId);
+	}, []);
 	return <AppRoutes />;
 };
 export default App;
